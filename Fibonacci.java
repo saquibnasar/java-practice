33d7d62ba65185e69797ea4d6b399a38521de7c3
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-01-17 06:59:00 +0530

    new change added

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please Enter a number ");
+        int num = in.nextInt();
+        int f = 0;
+        int l = 1;
+        for (int j = 0; j < num - 1; j++) {
+
+            int prevl = l;
+            l = f + l;
+
+            f = prevl;
+
+            System.out.println(l);
+        }
+        System.out.println(l);
+    }
+}
---
+++ Occurences.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Occurences {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please Enter a number ");
+        int num = in.nextInt();
+        int comprareNum = 13454324;
+
+        int loop = String.valueOf(comprareNum).length();
+
+        int nunberCount = 0;
+
+        for (int i = 0; i < loop; i++) {
+
+            int type = comprareNum % 10;
+            comprareNum = comprareNum / 10;
+
+            if (type == num) {
+                nunberCount = nunberCount + 1;
+            }
+
+        }
+        System.out.println(nunberCount);
+    }
+
+}
