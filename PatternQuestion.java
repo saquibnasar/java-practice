b82d1c60bb33ed8b066ea4db2b24eb788a2e434c
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-03-29 13:15:55 +0530

    new changes added

--- PatternQuestion.java
+++ PatternQuestion.java
@@ -1,7 +1,8 @@
 public class PatternQuestion {
     public static void main(String[] args) {
 
-        PatternQuestion(5);
+        // PatternQuestion(5);
+        PatternQuestion1(5);
     }
 
     static void PatternQuestion(int n) {
@@ -11,6 +12,17 @@ public class PatternQuestion {
             }
 
             System.out.println(" ");
+
+        }
+    }
+
+    static void PatternQuestion1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
 
         }
     }
