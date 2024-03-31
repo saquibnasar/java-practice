7e923cb828e87054e37612f1ae0a444e38a16618
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-03-31 18:49:06 +0530

    new changes added

--- PatternQuestion.java
+++ PatternQuestion.java
@@ -1,12 +1,14 @@
 public class PatternQuestion {
     public static void main(String[] args) {
 
-        // PatternQuestion(5);
+        // PatternQuestion5(5);
         // PatternQuestion1(5);
-        PatternQuestion2(5);
+        // PatternQuestion3(5);
+        // PatternQuestion4(5);
+        PatternQuestion6(5);
     }
 
-    static void PatternQuestion(int n) {
+    static void PatternQuestion5(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("* ");
@@ -19,7 +21,7 @@ public class PatternQuestion {
 
     static void PatternQuestion1(int n) {
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
 
@@ -39,4 +41,42 @@ public class PatternQuestion {
         }
     }
 
+    static void PatternQuestion3(int n) {
+        for (int i = 0; i <= n; i++) {
+            for (int j = 1; j < i + 1; j++) {
+
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+
+        }
+    }
+
+    static void PatternQuestion4(int n) {
+        for (int i = 0; i < 2 * n; i++) {
+            int col = i > n ? 2 * n - i : i;
+            for (int j = 0; j < col; j++) {
+                System.out.print("* ");
+            }
+
+            System.out.println();
+
+        }
+    }
+
+    static void PatternQuestion6(int n) {
+        for (int i = 0; i < 2 * n; i++) {
+            int col = i > n ? 2 * n - i : i;
+            int space = n - col;
+            for (int j = 0; j < space; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < col; j++) {
+                System.out.print(" *");
+            }
+            System.out.println();
+
+        }
+    }
 }
