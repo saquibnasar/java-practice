bc1ca69356fb015fa1c2bbf9a504341446b388cb
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-04-02 18:02:27 +0530

    new changes added

---
+++ Recursion.java
@@ -0,0 +1,13 @@
+public class Recursion {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    static void print(int n) {
+        System.out.println(n);
+        if (n == 5) {
+            return;
+        }
+        print(n + 1);
+    }
+}
