349c222c2593c9bb7439532cc168a77ce4319c43
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-04-08 22:26:07 +0530

    new changes added

--- BitwiseOperators.java
+++ BitwiseOperators.java
@@ -3,18 +3,31 @@
  */
 public class BitwiseOperators {
     public static void main(String[] args) {
+
+        int[] arr = { 4, 6, 7, 8, 9, 43, 234 };
+        int target = 10;
+        int ans = celingOfANumber(arr, target);
+        System.out.println(ans);
     }
 
-    static int biitwiseOperator() {
-        return -1;
-    }
+    static int celingOfANumber(int[] arr, int target) {
 
-    static int bigO() {
-        return -1;
-    }
+        int start = 0;
+        int end = arr.length - 1;
+        int mid = 0;
 
-    static int ques1() {
-        return -1;
-    }
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target == arr[mid]) {
+                return arr[mid];
+            }
+        }
 
+        return arr[start];
+
+    }
 }
