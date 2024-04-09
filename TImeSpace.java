ea24b2bc4a2fe8b873faea7c2d1d52c306b2e2c4
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-04-09 23:55:51 +0530

    new changes added

--- TImeSpace.java
+++ TImeSpace.java
@@ -2,19 +2,24 @@ import java.util.Arrays;
 
 public class TImeSpace {
     public static void main(String[] args) {
-
         int[] arr1 = { 1, 3 };
-        int[] arr2 = { 2 };
+        int[] arr2 = { 2, 4, 5, 6 };
 
         search(arr1, arr2);
 
     }
 
-    static int search(int[] m, int[] n) {
-
+    static void search(int[] m, int[] n) {
         int[] arr = merge(m, n);
-        System.out.println(Arrays.toString(arr));
-        return -1;
+        if (arr.length % 2 == 0) {
+            int value1 = arr[(arr.length / 2) - 1];
+            int value2 = arr[(arr.length) / 2];
+            float ans = (value1 + value2) / 2f;
+            System.out.print(ans);
+        } else {
+            float ans = arr[((arr.length + 1) / 2) - 1];
+            System.out.println(ans);
+        }
 
     }
 
@@ -24,19 +29,17 @@ public class TImeSpace {
         int arr2L = n.length;
         int arr3L = arr1L + arr2L;
         int[] arr3 = new int[arr3L];
-
-        for (int i = 0; i < (arr1L + arr2L) - 1; i = i + 1) {
-
-            int size1 = arr1L;
-            int size2 = arr2L;
-
-            if (m[i] < n[i]) {
-                arr3[i] = m[i];
-            } else {
-                arr3[i] = n[i];
+        int size1 = 0;
+        int size2 = 0;
+        for (int i = 0; i < (arr1L + arr2L); i = i + 1) {
+            if (size1 < arr1L && (size2 >= arr2L || m[size1] < n[size2])) {
+                arr3[i] = m[size1];
+                size1++;
+            } else if (size2 < arr2L) {
+                arr3[i] = n[size2];
+                size2++;
             }
         }
-
         return arr3;
 
     }
