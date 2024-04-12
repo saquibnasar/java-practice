4a9421901b9f9282d2809e4073ba68f46dffc4ce
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-04-12 23:35:17 +0530

    Palindrome java added

--- Palindrome.java
+++ Palindrome.java
@@ -1,5 +1,21 @@
 public class Palindrome {
     public static void main(String[] args) {
+        int num = 121;
+        System.out.println(palindrome(num));
+    }
+
+    static boolean palindrome(int num) {
+        String origin = String.valueOf(num);
+        String newNum = "";
+        for (int i = 0; i < origin.length(); i++) {
+            int singleInt = (num % 10);
+            num = num / 10;
+            newNum = newNum + String.valueOf(singleInt);
+        }
+        if (origin.equals(newNum)) {
+            return true;
+        }
+        return false;
 
     }
 }
