76ad4f706a370263f41b253ed8198cd83ae914d3
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-04-16 20:24:39 +0530

    new changes added

--- PrettyPrinting.java
+++ PrettyPrinting.java
@@ -3,7 +3,7 @@ public class PrettyPrinting {
         float a = 453.1274f;
         // System.out.printf("Formatted number is %.2f", a);
         // System.out.println(Math.PI);
-        // System.out.printf("Pie : %.3f", Math.PI);
+        System.out.printf("Pie : %.3f", Math.PI);
         System.out.println('a' + 'b');
         Character b = 'a';
         System.out.println(b.toString());
