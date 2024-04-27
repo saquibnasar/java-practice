8bd01a0ee73ac3e4efa8b77fe3ce82751deb404c
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-04-27 19:01:06 +0530

    linear search all the number that equal to target using recursion and arraylist where list is in the method

--- LinearSearchRecursion.java
+++ LinearSearchRecursion.java
@@ -1,5 +1,33 @@
+import java.util.ArrayList;
+
 public class LinearSearchRecursion {
     public static void main(String[] args) {
+        int[] arr = { 1, 4, 4, 8 };
+        int target = 4;
+        System.out.println(linearSearchAll(arr, 0, target));
+    }
+
+    static int linearSearch(int[] arr, int i, int target) {
+        if (arr[i] == target) {
+            return i;
+        }
+        if (arr.length == i + 1) {
+            return -1;
+        }
+        return linearSearch(arr, i + 1, target);
+    }
+
+    static ArrayList<Integer> linearSearchAll(int[] arr, int i, int target) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (arr.length == i) {
+            return list;
+        }
+        if (arr[i] == target) {
+            list.add(i);
+        }
+        ArrayList<Integer> ans = linearSearchAll(arr, i + 1, target);
+        list.addAll(ans);
+        return list;
 
     }
 }
