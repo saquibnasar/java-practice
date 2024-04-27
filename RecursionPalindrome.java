158a580c2f99f37a049b99c0e6f36f6e1eeee2b3
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-04-27 09:54:57 +0530

    palindrome solution using recurstion

--- RecursionPalindrome.java
+++ RecursionPalindrome.java
@@ -1,18 +1,21 @@
 public class RecursionPalindrome {
     public static void main(String[] args) {
 
-        int num = 12321;
-
-        recursionPalindrome(num);
-    }
-
-    static boolean recursionPalindrome(int num) {
-
-        return true;
+        int num = 121;
+        String n = Integer.toString(num);
+        System.out.println(recursionPalindrome(n, 0, n.length() - 1));
 
     }
 
-    static int helper(int num) {
-        return 1;
+    static boolean recursionPalindrome(String num, int s, int n) {
+
+        if (!(num.charAt(s) == num.charAt(n))) {
+            return false;
+        }
+        if (s == n || n < s) {
+            return true;
+
+        }
+        return recursionPalindrome(num, s + 1, n - 1);
     }
 }
