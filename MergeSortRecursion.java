e217db27cfab3a65374d235c0b3b9af4b71c9dba
Saquib Nasar Siddiqui <saquibsiddiqui023@gmail.com>
2024-05-03 23:42:34 +0530

    merge sort recursion

--- MergeSortRecursion.java
+++ MergeSortRecursion.java
@@ -1,9 +1,40 @@
+import java.util.Arrays;
+
 public class MergeSortRecursion {
     public static void main(String[] args) {
 
+        int[] arr1 = { 3, 9, 5 };
+        int[] arr2 = { 4, 6, 8 };
+        int[] arr = { 3, 9, 5, 6, 4, 8 };
+        int[] ans = new int[arr1.length + arr2.length];
+
+        // System.err.println(Arrays.toString(merge(arr1, arr2, ans, 0, 0)));
+        System.err.println(Arrays.toString(sort(arr, 0, arr.length / 2)));
     }
 
-    static void merge() {
+    static int[] sort(int[] arr, int size1, int size2) {
 
+        return arr;
+    }
+
+    static int[] merge(int[] arr1, int[] arr2, int[] merge, int count1, int count2) {
+
+        if (arr1.length <= count1 && arr2.length <= count2) {
+            return merge;
+        }
+        if (arr2.length <= count2) {
+            merge[count1 + count2] = arr1[count1];
+            return merge(arr1, arr2, merge, count1 + 1, count2);
+        } else if (arr1.length <= count1) {
+            return merge(arr1, arr2, merge, count1, count2 + 1);
+        }
+
+        if (arr1[count1] < arr2[count2] || arr2.length <= count2) {
+            merge[count1 + count2] = arr1[count1];
+            return merge(arr1, arr2, merge, count1 + 1, count2);
+        } else {
+            merge[count1 + count2] = arr2[count2];
+            return merge(arr1, arr2, merge, count1, count2 + 1);
+        }
     }
 }
